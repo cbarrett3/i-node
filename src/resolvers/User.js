a43a48993bccab90bf2082ed8bc0b080aab1851a
Connor Barrett <connor.steven.barrett@gmail.com>
2020-04-19 11:35:16 -0500

    User resolver finished

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,7 +1,11 @@
 function posts(parent, args, context) {
-    return context.prisma.user({ id: parent.id }).posts()
+    return context.prisma.user.findOne({ 
+        where: {
+            id: parent.id
+        },
+    }).posts()
 }
-  
+
 module.exports = {
     posts,
 }
